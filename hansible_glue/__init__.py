8c5d3804734bd89149ad8554602117e4218bcdb2
Jonas Gunz <himself@jonasgunz.de>
2022-02-25 17:59:54 +0100

    init

---
+++ hansible_glue/__init__.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+import os
+
+from ansible.cli.playbook import PlaybookCLI
+from ansible.release import __version__
+
+def version_ok():
+    return __version__.startswith('2.12')
+
+def run_playbook(_pb: str, _limit: str|None = None, _tags: str|None = None):
+    if not version_ok():
+        print('Ansible version not OK!')
+        return -1
+
+    pbcli = PlaybookCLI(['ansible-playbook', _pb])
+    return pbcli.run()
+
+if __name__ == '__main__':
+    run_playbook('pb.yml')
---
+++ setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+import setuptools
+
+setup(
+    name='hansible-glue',
+    version='0.0.0-dev',
+    author="Jonas Gunz",
+    author_mail="himself@jonasgunz.de",
+    description="Python glue for hansible",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "ansible>=2.12",
+    ],
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
+
